ac34b866aa1202311a7258a08568852361c235d8
davidkim4 <dkim4016@gmail.com>
2020-10-26 16:11:48 -0700

    initial commit

---
+++ app/javascript/components/mockApartments.js
@@ -0,0 +1,39 @@
+let mockApts = [
+    {
+        id: 1,
+        street: "1111 One St",
+        city: "Monstadt",
+        state: "Germany",
+        manager: "Jean",
+        email: "jean@ms.com",
+        price: "1000",
+        bedrooms: 2,
+        bathrooms: 2,
+        pets: "yes"
+    }
+    {
+        id: 2,
+        street: "1111 One St",
+        city: "Liyue",
+        state: "China",
+        manager: "Zhongli",
+        email: "zhongli@ly.com",
+        price: "2000",
+        bedrooms: 2,
+        bathrooms: 2,
+        pets: "no"
+    }
+    {
+        id: 3,
+        street: "1111 One St",
+        city: "Inazuma",
+        state: "Japan",
+        manager: "Ayaka",
+        email: "ayaka@iz.com",
+        price: "3000",
+        bedrooms: 2,
+        bathrooms: 2,
+        pets: "no"
+    }
+]
+export default mockApts
---
+++ app/javascript/components/pages/ApartmentShow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ApartmentShow extends Component {
+    render() {
+        return (
+            <>
+                <h3>Apartment show</h3>
+            </>
+        )
+    }
+}
